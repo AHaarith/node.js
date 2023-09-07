c3f8f41e232c499d9440ca01c2d64960bf32c1e2
Haarith Ahmed <Haarith.ahmed13@gmail.com>
2023-09-07 18:26:51 -0400

    first commit

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-var element = React.createElement('h1', { className: 'greeting' }, 'Hello, world!');
+const element = React.createElement('h1', { className: 'greeting' }, 'Hello, world!');
 ReactDOM.render(element, document.getElementById('root'));
 
 reportWebVitals();
